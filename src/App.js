abd472b23a8bcbb3d597b6a4eab52382a751c5fe
jordles113 <jordan.t.romero@gmail.com>
2022-03-23 15:09:57 -0500

    wires up tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         Dinosaurs! 
-        </p>
-      </header>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
